fb500ae28405550a4bf51dbaaa0ae699f9157894
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-06-26 00:38:36 -0300

    add monsters reducer in store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import monsterReducer from "./slices/monsterSlice"
+
 export const store = configureStore({
     reducer: {
-        
+        monsters: monsterReducer
     }
 })
